940432629f90d9c04263aaa244a97a94f031e693
Melvin David Rivera Monterroso (24036) <melvin.rivera@est.zamorano.edu>
2022-09-09 08:35:31 -0600

    Ejercicio 8.3

---
+++ Labs/Lab02/celsius2farenheit.py
@@ -0,0 +1,5 @@
+# Tendencias e innovacion en Tecnologias Agricolas
+
+celsius = input ("Ingrese temperatura en grados Celsius: ")
+farenheit = (float(celsius) * 9/5) + 32
+print ("La temperatura en grados Farenheit es: ", farenheit)
